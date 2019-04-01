b52393d59dabffefcf37b8dbf956173c084fe93b
changyu496 <changyu496@gmail.com>
2019-04-02 00:30:34 +0800

    [feature]增加了遍历的方法

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 const fs = require('fs')
 const babylon = require('babylon');
+const path = require('path');
 const traverse = require('babel-traverse').default;
 let ID = 0;
+
 function createAsset(filename){
   const content = fs.readFileSync(filename,'utf-8');
   const ast = babylon.parse(content,{
     sourceType:'module'
   })
-  const dependies = [];
+  const dependencies  = [];
   traverse(ast,{
     ImportDeclaration:({node})=>{
-      dependies.push(node.source.value);
+      dependencies.push(node.source.value);
     }
   })
   const id = ID++;
   return {
     id,
     filename,
-    dependies
+    dependencies
   }
 }
-const mainAsset = createAsset('./entry.js');
-console.log(mainAsset);
+
+function createGraph(entry){
+  mainAsset = createAsset(entry);
+  const queue = [mainAsset];
+  for(const asset of queue){
+    const dirname = path.dirname(asset.filename);
+    asset.mapping = {};
+    asset.dependencies.forEach(relativePath=>{
+      const absolutePath = path.join(dirname,relativePath);
+      const child = createAsset(absolutePath);
+      asset.mapping[relativePath] = child.id;
+      queue.push(child);
+    })
+  }
+  console.log(queue);
+  return queue;
+}
+
+const graph = createGraph('./entry.js');
